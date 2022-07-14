752ed5125496537ce9340df6c7c629a8ce0d2beb
volkov <volkovosnovnaya@gmail.com>
2022-07-14 23:50:54 +0300

    Добавил класс подключения к БД, вынес параметры  в .env

--- product/connect.js
+++ product/connect.js
@@ -1,15 +1,40 @@
 const { Pool, Client } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'busApplication',
-    password: 'admin',
-    port: 5432,
-});
 
-pool.query('SELECT * FROM public.routes;').then((res, err) => {
-    console.log(res.rows);
-    pool.end();
-});
+class BdConnection {
+    connection = {
+        user: process.env.BD_user,
+        host: process.env.BD_host,
+        database: process.env.BD_database,
+        password: process.env.BD_password,
+        port: Number(process.env.BD_port),
+    }
+
+    constructor() {
+        
+        this.pool = this._createPool(this.connection);
+    }
+
+    _createPool(dbSettings) {
+        return new Pool(dbSettings)
+    }
+
+    async queryBd(sql) {
+        return await this.pool.query(sql).then((data,err) => {
+            this.pool.end();
+            return data
+        })
+    }
+}
+
+
+testConnection = new BdConnection()
+
+
+testConnection.queryBd('SELECT * FROM public.routes;').then((data) => {
+    console.log(data.rows);
+})
+
+
 
